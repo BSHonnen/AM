79ac32c56ab2cb59a8dd6b065c5eda2424858fd5
Noah Denier <noah.denier@gmail.com>
2019-01-29 00:13:18 -0500

    Cleaned up code, added Comments, and removed magic numbers

--- TwitterPolling/poll-trends.py
+++ TwitterPolling/poll-trends.py
@@ -1,67 +1,97 @@
-#!/usr/bin/env python
+#-----------------------------------------------------------------------
+#
+# Polls Twitter for top 50 trends in top 5 US cities, pairs them down 
+# into a nested dictionary, and removes duplicates
+#
+#-----------------------------------------------------------------------
 
 #-----------------------------------------------------------------------
-# twitter-trends
-#  - lists the current global trending topics
+#  import all the things
 #-----------------------------------------------------------------------
 
 from twitter import *
 
+
+
 #-----------------------------------------------------------------------
-# load our API credentials
+# load our API credentials and import sys... and append a "." ... 'cause
 #-----------------------------------------------------------------------
+
 import sys
 sys.path.append(".")
 import config
 
+
+
 #-----------------------------------------------------------------------
 # create twitter API object
 #-----------------------------------------------------------------------
+
 twitter = Twitter(auth = OAuth(config.access_key,
                   config.access_secret,
                   config.consumer_key,
                   config.consumer_secret))
 
+
+
 #-----------------------------------------------------------------------
-# retrieve global trends.
-# other localised trends can be specified by looking up WOE IDs:
-#   http://developer.yahoo.com/geo/geoplanet/
-# twitter API docs: https://dev.twitter.com/rest/reference/get/trends/place
+# Where on Earth (WOE) IDs for:
+# 0 - New York, 1 - LA, 2 - Chicago, 3 - Dallas, 4 - DC
+# NOTE: List can be expanded but API limit is 75 requests every 15 minutes
 #-----------------------------------------------------------------------
-ny_results = twitter.trends.place(_id = 2459115)
 
-la_results = twitter.trends.place(_id = 2442047)
+top_USA_cities_by_capita = [2459115, 2442047, 2379574, 2388929, 2514815]
 
-chicago_results = twitter.trends.place(_id = 2379574)
 
-dallas_results = twitter.trends.place(_id = 2388929)
 
-dc_results = twitter.trends.place(_id = 2514815)
+#-----------------------------------------------------------------------
+# Pull each city's top 50 trends in this data structure: https://developer.twitter.com/en/docs/trends/trends-for-location/api-reference/get-trends-place
+#-----------------------------------------------------------------------
 
-all_results = ny_results+la_results+chicago_results+dallas_results+dc_results
-# print(all_results)
+top_trends = []
 
-# create an empty dictionary to store results without duplicates from each areas
+for city in top_USA_cities_by_capita:
+ top_trends.append(twitter.trends.place(_id = city))
+
+
+
+#-----------------------------------------------------------------------
+# Create an empty dictionary to store results without duplicates from each areas and variables for clarity
+#-----------------------------------------------------------------------
+
 paired_down = {}
+no_tweet_volume = "N/A"
 
-for location in all_results:
-	for trend in location ["trends"]:
-		if trend["name"] not in paired_down:
-			if trend["tweet_volume"] is None:
- 				paired_down[trend["name"]] = ['N/A']
-			else:
-				paired_down[trend["name"]] = [trend["tweet_volume"]]
-		else:
-			if trend["tweet_volume"] is None:
-				paired_down[trend["name"]].append(['N/A;])
-			else:
-				paired_down[trend["name"]].append([trend["tweet_volume"]])
- 		# print(" - %s" % trend["name"])
+
+
+#-----------------------------------------------------------------------
+# Removes duplicate names and notes the tweet volume from each trend
+#-----------------------------------------------------------------------
+
+for each_city in top_trends:
+ for location in each_city:
+  for trend in location["trends"]:
+   trend_name = trend["name"]
+   tweet_volume = trend["tweet_volume"]
+   if trend_name not in paired_down:
+    if tweet_volume is None:
+     paired_down[trend_name] = [no_tweet_volume]
+    else:
+     paired_down[trend_name] = [tweet_volume]
+   else:
+    if tweet_volume is None:
+     paired_down[trend_name].append([no_tweet_volume])
+    else:
+     paired_down[trend_name].append(tweet_volume)
+
+
+
+#-----------------------------------------------------------------------
+# Only show trends that appear in all 5 locales
+#-----------------------------------------------------------------------
 
 topics_appear_in_each = {}
 for trend in paired_down:
-	if len(trend) == 5:
-		topics_appear_in_each[trend]=trend
-	else:
-		pass
-print(topics_appear_in_each) 
+ if len(trend) == 5:
+  topics_appear_in_each[trend]=trend
+print(topics_appear_in_each)
